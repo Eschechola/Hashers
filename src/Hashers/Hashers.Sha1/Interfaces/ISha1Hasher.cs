e0dbef6f2dbb83a82c99683cb8a3ed465fb709b1
Lucas Eschechola <Lucas.eschechola@rockcontent.com>
2021-08-02 23:10:44 -0300

    Add Sha1

---
+++ src/Hashers/Hashers.Sha1/Interfaces/ISha1Hasher.cs
@@ -0,0 +1,8 @@
+﻿namespace Hashers.Interfaces
+{
+    public interface ISha1Hasher
+    {
+        string Hash(string password);
+        bool VerifyHashedPassword(string password, string hashedPassword);
+    }
+}
---
+++ src/Hashers/Hashers.Sha1/Services/Sha1Hasher.cs
@@ -0,0 +1,30 @@
+﻿using Hashers.Interfaces;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hashers.Services
+{
+    public class Sha1Hasher : ISha1Hasher
+    {
+        private readonly SHA1CryptoServiceProvider sha1CryptoTransform;
+
+        public Sha1Hasher()
+        {
+            sha1CryptoTransform = new SHA1CryptoServiceProvider();
+        }
+
+        public string Hash(string text)
+        {
+            byte[] buffer = Encoding.Default.GetBytes(text);
+            string hashedPassword = BitConverter.ToString(sha1CryptoTransform.ComputeHash(buffer))
+                .Replace("-", "")
+                .ToLower();
+
+            return hashedPassword;
+        }
+
+        public bool VerifyHashedPassword(string password, string hashedPassword)
+            => hashedPassword == Hash(password);
+    }
+}
